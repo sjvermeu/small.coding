69d077e103c35a49762179d903945d8a867cf18e
Sven Vermeulen <sven.vermeulen@siphos.be>
2011-08-16 22:39:10 +0000

    Remove traces of BASEDIR

--- se_scripts/segenfix.sh
+++ se_scripts/segenfix.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-typeset BASEDIR=/home/swift/Development/build
-typeset ORIGSRC=refpolicy-20110726/policy/modules;
-typeset NEWDST=../Centralized/hardened-refpolicy/policy/modules;
-typeset PATCHES=../Centralized/small.coding/selinux-modules/patches;
-typeset TMPLOC=tmp/segenfix_policy;
+ORIGSRC=/home/swift/Development/build/refpolicy-20110726/policy/modules;
+NEWDST=/home/swift/Development/Centralized/hardened-refpolicy/policy/modules;
+PATCHES=/home/swift/Development/Centralized/small.coding/selinux-modules/patches;
+TMPLOC=/home/swift/Development/build/tmp/segenfix_policy;
 
 typeset TARGET=$2;
 typeset TYPE=$1;
@@ -21,11 +20,11 @@ then
   exit 1;
 fi
 
-if [ -f "${BASEDIR}/${ORIGSRC}/${TARGET}.te" ];
+if [ -f "${ORIGSRC}/${TARGET}.te" ];
 then
   SUBJECT="${TARGET}";
 else
-  SUBJECT=$(find ${BASEDIR}/${NEWDST} -type f -name ${TARGET}.te 2>/dev/null | awk -F'/' '{print $(NF-1)"/"$NF}' | sed -e "s:.te$::g");
+  SUBJECT=$(find ${NEWDST} -type f -name ${TARGET}.te 2>/dev/null | awk -F'/' '{print $(NF-1)"/"$NF}' | sed -e "s:.te$::g");
   if [ "${SUBJECT}" = "" ];
   then
     SUBJECT=${TARGET};
@@ -34,47 +33,41 @@ fi
 
 if [ "${TYPE}" = "fullpath" ] && [ "${SUBJECT}" != "modules" ];
 then
-  cd ${BASEDIR};
   diff -uN ${ORIGSRC}/${SUBJECT}.te ${NEWDST}/${SUBJECT}.te | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
   diff -uN ${ORIGSRC}/${SUBJECT}.if ${NEWDST}/${SUBJECT}.if | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
   diff -uN ${ORIGSRC}/${SUBJECT}.fc ${NEWDST}/${SUBJECT}.fc | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
   [ -f ${NEWDST}/${SUBJECT}.te.in ] && diff -uN ${ORIGSRC}/${SUBJECT}.te.in ${NEWDST}/${SUBJECT}.te.in | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
 elif [ "${TYPE}" = "fullpath" ] && [ "${SUBJECT}" = "modules" ];
 then
-  cd ${BASEDIR};
   diff -uN ${ORIGSRC}/../modules.conf ${NEWDST}/../modules.conf;
 elif [ "${TYPE}" = "minimal" ];
 then
-  cd ${BASEDIR};
   diff -uN ${ORIGSRC}/${SUBJECT}.te ${NEWDST}/${SUBJECT}.te | sed -e 's:../Centralized/hardened-refpolicy/policy/modules/::g' -e 's:refpolicy.orig/policy/modules/::g';
   diff -uN ${ORIGSRC}/${SUBJECT}.fc ${NEWDST}/${SUBJECT}.fc | sed -e 's:../Centralized/hardened-refpolicy/policy/modules/::g' -e 's:refpolicy.orig/policy/modules/::g';
 elif [ "${TYPE}" = "incremental" ];
 then
-  if [ -d "${BASEDIR}/${TMPLOC}" ];
+  if [ -d "${TMPLOC}" ];
   then
-    echo "Failed - ${BASEDIR}/${TMPLOC} exists!";
+    echo "Failed - ${TMPLOC} exists!";
     exit 1;
   fi
   # Create a blank location
-  mkdir ${BASEDIR}/${TMPLOC};
-  rsync -aug ${BASEDIR}/${ORIGSRC}/../../ ${BASEDIR}/${TMPLOC};
+  mkdir ${TMPLOC};
+  rsync -aug ${ORIGSRC}/../../ ${TMPLOC};
   # Patch the current location
-  pushd ${BASEDIR}/${TMPLOC} > /dev/null 2>&1;
-  for PATCH in ${BASEDIR}/${PATCHES}/*.patch;
+  pushd ${TMPLOC} > /dev/null 2>&1;
+  for PATCH in ${PATCHES}/*.patch;
   do
     patch -p1 < ${PATCH} > /dev/null 2>&1;
   done
   popd > /dev/null 2>&1;
-  pushd ${BASEDIR} > /dev/null 2>&1;
   # Now take the diff of the requested target
   diff -uN ${TMPLOC}/policy/modules/${SUBJECT}.te ${NEWDST}/${SUBJECT}.te | sed -e "s:${TMPLOC}:refpolicy.orig:g" | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
   diff -uN ${TMPLOC}/policy/modules/${SUBJECT}.if ${NEWDST}/${SUBJECT}.if | sed -e "s:${TMPLOC}:refpolicy.orig:g" | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
   diff -uN ${TMPLOC}/policy/modules/${SUBJECT}.fc ${NEWDST}/${SUBJECT}.fc | sed -e "s:${TMPLOC}:refpolicy.orig:g" | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
   [ -f ${NEWDST}/${SUBJECT}.te.in ] && diff -uN ${TMPLOC}/policy/modules/${SUBJECT}.te.in ${NEWDST}/${SUBJECT}.te.in | sed -e "s:${TMPLOC}:refpolicy.orig:g" | sed -e 's:../Centralized/hardened-refpolicy:refpolicy:g';
   # Clear working dir
-  popd > /dev/null 2>&1;
-  rm -rf ${BASEDIR}/${TMPLOC};
+  rm -rf ${TMPLOC};
 else
-  cd ${BASEDIR};
   diff -uN ${ORIGSRC}/../../${TARGET} ${NEWDST}/../../${TARGET} | sed -e 's:../Centralized/hardened-refpolicy/:refpolicy:g';
 fi

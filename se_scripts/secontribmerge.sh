e1f6a880a02cd6bb0d65504f899925c414a32206
Sven Vermeulen <sven.vermeulen@siphos.be>
2012-10-15 21:18:31 +0200

    Update to current state

--- se_scripts/secontribmerge.sh
+++ se_scripts/secontribmerge.sh
@@ -3,15 +3,16 @@
 SRC=/home/swift/Development/Centralized/refpolicy/policy/modules/contrib
 
 NUM=${1};
+PATCH=$(ls ${SRC}/${NUM}*.patch);
 
-git am ${SRC}/${NUM}*.patch;
+git am ${PATCH};
 
 if [ $? -ne 0 ];
 then
-	patch -p1 < ${SRC}/${NUM}*.patch;
+	patch -p1 < ${PATCH};
 	rm -f *.orig;
 	ls *.rej > /dev/null 2>&1;
-	if [ $? -ne 0 ];
+	if [ $? -eq 0 ];
 	then
 		echo "";
 		echo " Patch rejections found. Clean this up manually please."

b762bcd1aff3147b444ec8e2f12b3b4d558d1235
Sven Vermeulen <sven.vermeulen@siphos.be>
2013-08-20 23:48:09 +0200

    updates

--- mgl/modules/postgresql/functions.sh
+++ mgl/modules/postgresql/functions.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+psql_full() {
+  [ -n "${MGL_DEBUG}" ] && echo "### DEBUG: SQL statement = $*";
+  if [ -z "${PGUSER}" ];
+  then
+    psql -U postgres -c "$*";
+  else
+    psql -c "$*";
+  fi
+}
 
-
+psql_statement() {
+  [ -n "${MGL_DEBUG}" ] && echo "### DEBUG: SQL statement = $*" >&3;
+  if [ -z "${PGUSER}" ];
+  then
+    psql -t -A -U postgres -c "$*";
+  else
+    psql -t -A -c "$*";
+  fi
+}

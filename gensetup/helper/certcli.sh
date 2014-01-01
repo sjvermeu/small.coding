3a90c32f502d5c417ff11d5fe9046170818d9eed
Sven Vermeulen <sven.vermeulen@siphos.be>
2014-01-01 11:57:03 +0100

    Add listing suppot

--- gensetup/helper/certcli.sh
+++ gensetup/helper/certcli.sh
@@ -2,6 +2,7 @@
 
 export KEYLOC=${KEYLOC:=/etc/ssl};
 export RANDFILE=${RANDFILE:=${KEYLOC}/.rnd};
+export WORKSPACE=${WORKSPACE:=${KEYLOC}/workspace};
 
 die() {
   echo "!! $*";
@@ -10,7 +11,9 @@ die() {
 
 toparg="";
 createrootflag=0;
+listrootflag=0;
 createchildflag=0;
+listchildflag=0;
 parentflag=0;
 signflag=0;
 output="";
@@ -18,6 +21,7 @@ outputflag=0;
 valid="";
 validflag=0;
 requestflag=0;
+revokeflag=0;
 parent="";
 
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ];
@@ -26,30 +30,43 @@ then
   echo "";
   echo "Command can be one of:";
   echo "  -r, --create-root		Create a root CA key named <value>";
-  echo "  -c, --create-child		Create a child CA key named <value>";
+  echo "  -l, --list-root               List root CAs (self-signed CAs)";
+  echo "  -c, --create-child		Create a child CA key named <value> for parent";
+  echo "  -L, --list-child              List child CAs for given parent";
   echo "  -s, --sign-request		Sign a certificate request (<value> is input)";
   echo "  -R, --create-request		Create a key and signing request named <value>.req";
+  echo "  -x, --revoke                  Revoke the key matching <value>";
   echo "";
   echo "Options can be one of:";
   echo "  -p, --parent <parent>		Use <parent> as the parent key value";
   echo "  -o, --output <file>		Save resulting file as <file>";
   echo "  -v, --valid <days>		Number of days that the certificate is valid";
   echo "";
+  echo "Environment variables used:";
+  echo "  KEYLOC [/etc/ssl]             Base directory used by the script.";
+  echo "  RANDFILE [KEYLOC/.rnd]        Random file used by OpenSSL.";
+  echo "  WORKSPACE [KEYLOC/workspace]  Location where non-CA keys/certs are placed.";
   exit 1;
 fi
 
-eval set -- "$(getopt -n $(basename $0) -s sh -o rRcsp:o:v: --long create-root,create-request,create-child,sign-request,parent:,output:,valid: -- "$@")"
+eval set -- "$(getopt -n $(basename $0) -s sh -o rlRcLsxp:o:v: --long create-root,list-root,create-request,create-child,list-child,sign-request,revoke,parent:,output:,valid: -- "$@")"
 while [ $# -gt 0 ];
 do
   case "$1" in
     (-r) createrootflag=1;;
     (--create-root) createrootflag=1;;
+    (-l) listrootflag=1;;
+    (--list-root) listrootflag=1;;
     (-R) requestflag=1;;
     (--create-request) requestflag=1;;
     (-c) createchildflag=1;;
     (--create-child) createchildflag=1;;
+    (-L) listchildflag=1;;
+    (--list-child) listchildflag=1;;
     (-s) signflag=1;;
     (--sign-request) signflag=1;;
+    (-x) revokeflag=1;;
+    (--revoke) revokeflag=1;;
     (-p) parentflag=1; parent="$2"; shift;;
     (--parent) parentflag=1; parent="$2"; shift;;
     (-o) outputflag=1; output="$2"; shift;;
@@ -63,8 +80,17 @@ do
   shift;
 done
 
+[ ! -d ${WORKSPACE} ] && (mkdir ${WORKSPACE} || die "Could not create workspace ${WORKSPACE}");
+
 if [ ${createrootflag} -eq 1 ];
 then
+  if [ -z "${toparg}" ];
+  then
+    echo "The name of the root certificate must be passed.";
+    echo "";
+    echo "Example: $0 -r genfic";
+    exit 1;
+  fi
   if [ -d ${KEYLOC}/${toparg} ];
   then
     echo "Location ${KEYLOC}/${toparg} already exists!";
@@ -150,6 +176,69 @@ EOF
   openssl ca -config ./openssl.cnf -gencrl -crldays 365 -keyfile private/root-${toparg}.key -cert root-${toparg}.crt -out root-${toparg}.crl
   popd > /dev/null 2>&1;
 fi
+
+if [ ${listrootflag} -eq 1 ];
+then
+  for DIR in ${KEYLOC}/*;
+  do
+    for FILE in ${DIR}/*.crt;
+    do
+      [ ! -f "${FILE}" ] && continue;
+      # See if the certificate is self-signed
+      DIFF=$(openssl x509 -noout -text -in ${FILE} | grep -E '(Issuer|Subject):' | sed -e 's:^[^\:]*::g' | sort | uniq | wc -l);
+      if [ "${DIFF}" = "1" ];
+      then
+        # Certificate is self-signed
+	CERTNAME=$(basename ${DIR});
+	CERTID=$(openssl x509 -noout -text -in ${FILE} | grep 'Subject:' | sed -e 's:^[^\:]*\:::g' | sed -e 's:^[ 	]*::g' -e 's:[ 	]*$::g');
+	EXPIRE=$(openssl x509 -noout -text -in ${FILE} | grep 'Not After :' | sed -e 's:^.*Not After \:::g');
+	echo "${CERTNAME}";
+	echo "  ${CERTID}";
+	echo "  expires ${EXPIRE}";
+      fi
+    done
+  done
+fi
+
+if [ ${listchildflag} -eq 1 ];
+then
+  if [ ${parentflag} -ne 1 ];
+  then
+    echo "You need to pass a parent key identifier";
+    echo "";
+    echo "Example: $0 -p genfic -L";
+    exit 1;
+  fi
+  # Get Subject from given parent
+  PARENTSUBJECT=$(openssl x509 -noout -text -in ${KEYLOC}/${parent}/root-${parent}.crt | grep 'Subject:' | sed -e 's:.*Subject\:::g');
+  # Now look for keys signed (Issuer:) by this subject
+
+  for DIR in ${KEYLOC}/*;
+  do
+    for FILE in ${DIR}/*.crt;
+    do
+      [ ! -f "${FILE}" ] && continue;
+      # See if the certificate is self-signed (if so, then no)
+      DIFF=$(openssl x509 -noout -text -in ${FILE} | grep -E '(Issuer|Subject):' | sed -e 's:^[^\:]*::g' | sort | uniq | wc -l);
+      if [ "${DIFF}" = "1" ];
+      then
+        continue;
+      fi
+      KEYISSUER=$(openssl x509 -noout -text -in ${FILE} | grep 'Issuer:' | sed -e 's:^.*Issuer\:::g');
+      if [ "${PARENTSUBJECT}" = "${KEYISSUER}" ];
+      then
+        # Certificate is signed by given parent
+	CERTNAME=$(basename ${DIR});
+	CERTID=$(openssl x509 -noout -text -in ${FILE} | grep 'Subject:' | sed -e 's:^[^\:]*\:::g' | sed -e 's:^[ 	]*::g' -e 's:[ 	]*$::g');
+	EXPIRE=$(openssl x509 -noout -text -in ${FILE} | grep 'Not After :' | sed -e 's:^.*Not After \:::g');
+	echo "${CERTNAME}";
+	echo "  ${CERTID}";
+	echo "  expires ${EXPIRE}";
+      fi
+    done
+  done
+fi
+
 
 if [ ${createchildflag} -eq 1 ];
 then
@@ -278,3 +367,17 @@ then
   echo "Running: openssl req -newkey rsa:2048 -keyout ${toparg}.key -out ${toparg}.req";
   openssl req -newkey rsa:2048 -keyout ${toparg}.key -out ${toparg}.req || die "OpenSSL failed";
 fi
+
+if [ ${revokeflag} -eq 1 ];
+then
+  if [ ${parentflag} -eq 0 ];
+  then
+    echo "Revoking certificates requires a parent key.";
+    exit 1;
+  fi
+  echo "##";
+  echo "## Revoking certificates";
+  echo "##";
+  echo "## TODO";
+fi
+  
